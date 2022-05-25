42781e8d97848274805220ee3c68ff12b3617265
ForHemer <foreste.hemerson@gmail.com>
2022-05-24 22:10:23 -0400

    Define and set up class properties in the constructor method

--- classes/game.rb
+++ classes/game.rb
@@ -1,5 +1,12 @@
 require_relative 'items'
 
 class Game < Item
+  attr_accessor :multiplayer, :last_played_at
 
+  def initialize(multiplayer, last_played_at, publish_date)
+    super(publish_date, archived)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+  end
+  
 end
