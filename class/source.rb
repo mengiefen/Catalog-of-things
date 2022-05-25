8c205737257e93b35eeec73f47203f8eb447abeb
Mauro Vasconcelos <88060989+vanillainanutshell@users.noreply.github.com>
2022-05-25 11:35:01 -0300

    Latest fixes

--- class/source.rb
+++ class/source.rb
@@ -1,8 +1,9 @@
 class Source
   attr_reader :name
+  attr_accessor :id
 
-  def initialize(id, name)
-    @id = id
+  def initialize(name)
+    @id = Random.rand(1..1000)
     @name = name
     @items = []
   end
