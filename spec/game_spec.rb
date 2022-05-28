6dd1042d96677166e2fb48f3f1b72bd35a70b75c
andres-condezo <ra.condezomonge@gmail.com>
2022-05-27 22:05:44 -0500

    Fix game_spec test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,7 +3,7 @@ require_relative '../src/classes/game'
 describe 'Game' do
   context 'when create a new game' do
     it 'return a new object' do
-      game = Game.new('01/01/2000', false, '01/01/2005')
+      game = Game.new(rand(0..1000), '01/01/2000', false, '01/01/2005')
       expect(game).to be_an_instance_of(Game)
     end
   end
